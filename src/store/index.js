e0c904505802e129ed4e0142df00bdaa96d778d7
Antoine <antoine.pyre@slickteam.fr>
2022-03-14 15:09:01 +0100

    sort pokemons

--- src/store/index.js
+++ src/store/index.js
@@ -15,7 +15,12 @@ const store = createStore({
       const api = new PokemonClient();
 
       api.getPokemonByName(name)
-        .then((pokemon) => state.pokemons.list.push(pokemon));
+        .then((pokemon) => {
+          state.pokemons.list.push(pokemon)
+          state.pokemons.list.sort((a, b) => {
+            return a.id > b.id;
+          })
+        });
     },
     getNextPokemons(state, limit) {
       const api = new PokemonClient();
